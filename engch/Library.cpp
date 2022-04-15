10c14f3887df7c496fdfc612d62f543915a67fff
Ng eng chun <engchun@live.com.sg>
2022-04-15 10:51:04 +0800

    clean up

--- engch/Library.cpp
+++ engch/Library.cpp
@@ -15,19 +15,13 @@ using namespace Engch;
     return wasFirstTime; } () )
 
 void Library::changeCmdTextColor(int colorChoice) {
-    // change the next character color in cmd, 7 is default color
+    // highlight the next character with color in cmd, 7 is default color.
     // https://stackoverflow.com/questions/4053837/colorizing-text-in-the-console-with-c
     SetConsoleTextAttribute(this->hConsole, colorChoice);
-    // example
-//    printf("[");
-//    SetConsoleTextAttribute(hConsole, 10);
-//    printf("%c", 'x');
-//    SetConsoleTextAttribute(hConsole, 7);
-//    printf("]");
 }
 
 double Library::getRand(int range) {
-    // return random double up to
+    // return a random double from 0 to range
     // https://stackoverflow.com/questions/12937963/get-local-time-in-nanoseconds
     auto start = std::chrono::high_resolution_clock::now();
     using namespace std;
@@ -48,21 +42,6 @@ double Library::getDoubleModulus(double x, double y) {
 bool Library::isDoneBefore() {
     // run only once. return true is not run before, false if run before
     // https://stackoverflow.com/questions/8412630/how-to-execute-a-piece-of-code-only-once
-
-    // example --> no yes yes
-//    Engch::Library ec;
-//    std::string e = "no";
-//    printf("%s\n", e.c_str()); // no
-//
-//    if (ec.isDoneBefore()){
-//        e = "yes";
-//    }
-//    printf("%s\n", e.c_str()); // yes
-//
-//    if (ec.isDoneBefore()){ // never enter as it ran before already
-//        e = "no";
-//    }
-//    printf("%s\n", e.c_str()); // yes
     return FIRST_TIME_HERE;
 }
 
